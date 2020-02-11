e07f022466ab2db9b2b520b267823902fb46d33f
eliseunogueira <eliseu.nogueira@outlook.com>
2020-02-11 10:41:19 -0800

    slice the code with functions, and contracts at the top

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+void PrintIntro();
+string GetGuessAndPrintBack();
+// the entry point for our application
 int main()
+{
+  PrintIntro();
+  GetGuessAndPrintBack();
+  return 0;
+}
+
+// the implementation of the method
+void PrintIntro()
 {
   // Introduce the game
   constexpr int WORLD_LENGTH = 5;
@@ -9,12 +20,17 @@ int main()
   cout << "Can you guess the " << WORLD_LENGTH;
   cout << " letter isogram I'm thingking of\n";
   cout << endl;
+  return;
+}
 
-  // get a guess from the player
+// the implementation of getGuess
+// get a guess from the player
+string GetGuessAndPrintBack()
+{
   cout << "Please write your guess: ";
   string Guess = "";
-  cin >> Guess;
-  // report the guess back to them
+  getline(cin, Guess);
+  // print the guess guess back to them
   cout << "Your guess was: " << Guess;
-  return 0;
+  return Guess;
 }
